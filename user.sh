7756e50c68dbed73a95f7c8345643aa5c44cf078
ChillerDragon <ChillerDragon@gmail.com>
2024-03-18 10:09:00 +0800

    Add chiller user chiller style

--- user.sh
+++ user.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+set -euo pipefail
 
-addgroup teeworlds
-adduser \
-  --comment "" \
-  --home /home/teeworlds \
-  --shell /bin/bash \
-  --disabled-password \
-  --ingroup users teeworlds
+if ! grep -q '^teeworlds:' /etc/passwd
+then
+  addgroup teeworlds
+  adduser \
+    --comment "" \
+    --home /home/teeworlds \
+    --shell /bin/bash \
+    --disabled-password \
+    --ingroup users teeworlds
+fi
+
+if ! grep -q '^chiller:' /etc/passwd
+then
+  useradd \
+    --shell /bin/bash \
+    --create-home \
+    --home-dir /home/chiller chiller
+fi
+
+printf '[*] done all users created.\n'
 
