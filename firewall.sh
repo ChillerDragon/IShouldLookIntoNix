5c08e55b2a87b06a64c15138d107850a590066f9
ChillerDragon <ChillerDragon@gmail.com>
2024-03-18 20:06:35 +0800

    ddnet styled firewall

--- firewall.sh
+++ firewall.sh
@@ -16,11 +16,29 @@ iptables-save > "$backup_file"
 
 printf '[*] backed up iptables to %s\n' "$backup_file"
 
-# delete all current rules
-iptables --flush
-iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A INPUT -p udp 8303 -j ACCEPT
-iptables -A INPUT -p tcp --dport "$SSH_PORT" -j ACCEPT
-iptables -A INPUT -p tcp -j DROP
+# deletes all current rules
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -t nat -F
+iptables -t mangle -F
+iptables -F
+iptables -X
+iptables -t raw -A PREROUTING -p udp -j NOTRACK
+iptables -t raw -A OUTPUT -p udp -j NOTRACK
+iptables -N serverinfo
+iptables -N newconn
+iptables -A INPUT -p udp -m u32 --u32 "38=0x67696533" -j serverinfo
+iptables -A INPUT -p udp -m u32 --u32 "38=0x66737464" -j serverinfo
+iptables -A INPUT -p udp -m u32 --u32 "32=0x544b454e" -j newconn
+iptables -A serverinfo -s 37.187.108.123 -j ACCEPT
+iptables -A serverinfo -m hashlimit --hashlimit-above 100/s --hashlimit-burst 250 --hashlimit-mode dstport --hashlimit-name si_dstport -j DROP
+iptables -A serverinfo -m hashlimit --hashlimit-above 20/s --hashlimit-burst 100 --hashlimit-mode srcip --hashlimit-name si_srcip -j DROP
+iptables -A newconn -m hashlimit --hashlimit-above 100/s --hashlimit-burst 100 --hashlimit-mode dstport --hashlimit-name nc_dstport -j DROP
+iptables -A INPUT -s 127.0.0.1 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp -m conntrack -m multiport --ctstate NEW ! --dports "$SSH_PORT" -j DROP
+iptables -A INPUT -p udp -m udp --dport 8303 -j ACCEPT
+iptables -A INPUT -p udp -m udp --dport 53 -j ACCEPT
+iptables -A INPUT -p udp -j DROP
+
 
