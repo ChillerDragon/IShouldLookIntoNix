55bc3130e3b0f1ad42459a2c946eca5228ecf0a2
ChillerDragon <ChillerDragon@gmail.com>
2024-03-18 10:26:31 +0800

    Ewww interactive

--- main.sh
+++ main.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+set -euo pipefail
+
+printf 'enter your ssh public key:\n'
+read -e -r -p '> ' SSH_PUBLIC_KEY
+
 ./packages.sh
 ./firewall.sh
-./user.sh
+./user.sh "$SSH_PUBLIC_KEY"
+
+if [ ! -f /root/.ssh/authorized_keys ]
+then
+  mkdir -p /root/.ssh
+  printf '%s\n' "$SSH_PUBLIC_KEY" >> /root/.ssh/authorized_keys
+fi
 
--- user.sh
+++ user.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+#
+# usage: users.sh SSH_PUBLIC_KEY
+#
+
 set -euo pipefail
+
+SSH_PUBLIC_KEY="${SSH_PUBLIC_KEY:-$1}"
+if [ "$SSH_PUBLIC_KEY" = "" ]
+then
+  printf '%s\n' "usage: users.sh SSH_PUBLIC_KEY" 1>&2
+  exit 1
+fi
 
 if ! grep -q '^teeworlds:' /etc/passwd
 then
@@ -10,6 +21,15 @@ then
     --shell /bin/bash \
     --disabled-password \
     --ingroup users teeworlds
+  chown -R teeworlds:admin /home/teeworlds/
+  chmod o-x /home/teeworlds/
+fi
+
+if [ ! -f /home/teeworlds/.ssh/authorized_keys ]
+then
+  mkdir -p /home/teeworlds/.ssh
+  chown teeworlds:teeworlds /home/chiller/.ssh/
+  printf '%s\n' "$SSH_PUBLIC_KEY" > /home/teeworlds/.ssh/authorized_keys
 fi
 
 if ! grep -q '^chiller:' /etc/passwd
@@ -20,6 +40,14 @@ then
     --create-home \
     --groups admin \
     --home-dir /home/chiller chiller
+  chmod o-x /home/chiller
+fi
+
+if [ ! -f /home/chiller/.ssh/authorized_keys ]
+then
+  mkdir -p /home/chiller/.ssh
+  chown chiller:chiller /home/chiller/.ssh/
+  printf '%s\n' "$SSH_PUBLIC_KEY" > /home/chiller/.ssh/authorized_keys
 fi
 
 printf '[*] done all users created.\n'
