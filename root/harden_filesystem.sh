9607a938de7f816e777237d9846bc1b64bd06a8e
ChillerDragon <ChillerDragon@gmail.com>
2024-03-20 12:36:30 +0800

    log

--- root/harden_filesystem.sh
+++ root/harden_filesystem.sh
@@ -4,10 +4,12 @@ set -euo pipefail
 
 restricted_dirs=(/usr/games /usr/src /etc /media /opt /srv /var)
 
+printf '[*] removing read access for %s\n' "${restricted_dirs[*]}"
 chown -R root:admin "${restricted_dirs[@]}"
 chmod o-x "${restricted_dirs[@]}"
 chmod o-r "${restricted_dirs[@]}"
 
+printf '[*] allwowing read access for /etc/alternatives and /etc/profile\n'
 chmod o+r /etc/alternatives
 chmod o+r /etc/profile
 
