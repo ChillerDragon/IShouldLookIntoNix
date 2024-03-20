36141b9556acb74bab6fc2a6efe4e3242722a3e2
ChillerDragon <ChillerDragon@gmail.com>
2024-03-20 10:37:55 +0800

    flawed groups check

--- root/user.sh
+++ root/user.sh
@@ -14,53 +14,53 @@ set -euo pipefail
 SSH_PUBLIC_KEY="${SSH_PUBLIC_KEY:-$1}"
 if [ "$SSH_PUBLIC_KEY" = "" ]
 then
-  printf '%s\n' "usage: users.sh SSH_PUBLIC_KEY" 1>&2
-  exit 1
+	printf '%s\n' "usage: users.sh SSH_PUBLIC_KEY" 1>&2
+	exit 1
 fi
 
-if ! groups | grep -q admin
+if ! grep -q '^admin:' /etc/group
 then
-  addgroup admin
-printf '[*] created admin group .. OK\n'
+	addgroup admin
+	printf '[*] created admin group .. OK\n'
 fi
 
 if ! grep -q '^teeworlds:' /etc/passwd
 then
-  addgroup teeworlds
-  adduser \
-    --comment "" \
-    --home /home/teeworlds \
-    --shell /bin/bash \
-    --disabled-password \
-    --ingroup users teeworlds
-  chown -R teeworlds:admin /home/teeworlds/
-  chmod o-x /home/teeworlds/
-  printf '[*] created user teeworlds .. OK\n'
+	addgroup teeworlds
+	adduser \
+		--comment "" \
+		--home /home/teeworlds \
+		--shell /bin/bash \
+		--disabled-password \
+		--ingroup users teeworlds
+			chown -R teeworlds:admin /home/teeworlds/
+			chmod o-x /home/teeworlds/
+			printf '[*] created user teeworlds .. OK\n'
 fi
 
 if [ ! -f /home/teeworlds/.ssh/authorized_keys ]
 then
-  mkdir -p /home/teeworlds/.ssh
-  chown teeworlds:teeworlds /home/chiller/.ssh/
-  printf '%s\n' "$SSH_PUBLIC_KEY" > /home/teeworlds/.ssh/authorized_keys
+	mkdir -p /home/teeworlds/.ssh
+	chown teeworlds:teeworlds /home/chiller/.ssh/
+	printf '%s\n' "$SSH_PUBLIC_KEY" > /home/teeworlds/.ssh/authorized_keys
 fi
 
 if ! grep -q '^chiller:' /etc/passwd
 then
-  useradd \
-    --shell /bin/bash \
-    --create-home \
-    --groups admin sudo \
-    --home-dir /home/chiller chiller
-  chmod o-x /home/chiller
-  printf '[*] created user chiller .. OK\n'
+	useradd \
+		--shell /bin/bash \
+		--create-home \
+		--groups admin sudo \
+		--home-dir /home/chiller chiller
+			chmod o-x /home/chiller
+			printf '[*] created user chiller .. OK\n'
 fi
 
 if [ ! -f /home/chiller/.ssh/authorized_keys ]
 then
-  mkdir -p /home/chiller/.ssh
-  chown chiller:chiller /home/chiller/.ssh/
-  printf '%s\n' "$SSH_PUBLIC_KEY" > /home/chiller/.ssh/authorized_keys
+	mkdir -p /home/chiller/.ssh
+	chown chiller:chiller /home/chiller/.ssh/
+	printf '%s\n' "$SSH_PUBLIC_KEY" > /home/chiller/.ssh/authorized_keys
 fi
 
 printf '[*] done all users created.\n'
