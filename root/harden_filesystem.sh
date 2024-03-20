315f682412c2b957d8ea739c6b37218ecf7276fc
ChillerDragon <ChillerDragon@gmail.com>
2024-03-20 12:33:07 +0800

    meh usr is also needed

--- root/harden_filesystem.sh
+++ root/harden_filesystem.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-restricted_dirs=(/usr /etc /media /opt /srv /var)
+restricted_dirs=(/usr/games /usr/src /etc /media /opt /srv /var)
 
 chown -R root:admin "${restricted_dirs[@]}"
 chmod o-x "${restricted_dirs[@]}"
