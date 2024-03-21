131f0b6ab3127addc2a929c4a46f7f477f561740
ChillerDragon <ChillerDragon@gmail.com>
2024-03-21 11:45:35 +0800

    clone scripts repo into user home

--- root/user_tools.sh
+++ root/user_tools.sh
@@ -8,28 +8,40 @@ set -euo pipefail
 UNIX_USER="${1:-}"
 if [ "$UNIX_USER" = "" ]
 then
-  printf '%s\n' "usage: user_tools.sh UNIX_USER" 1>&2
-  exit 1
+	printf '%s\n' "usage: user_tools.sh UNIX_USER" 1>&2
+	exit 1
+fi
+
+if [[ "$UNIX_USER" =~ ^[a-z][a-z0-9]*$ ]]
+then
+	printf "[-] Error: invalid unix user name '%s' only [a-z] is allowed\n" "$UNIX_USER" 1>&2
+	exit 1
 fi
 
 if ! grep -q "^$UNIX_USER:" /etc/passwd
 then
-  printf '[-] Error: unix user not found %s\n' "$UNIX_USER" 1>&2
-  exit 1
+	printf '[-] Error: unix user not found %s\n' "$UNIX_USER" 1>&2
+	exit 1
 fi
 
 if [ ! -d "/home/$UNIX_USER" ]
 then
-  printf '[-] Error: directory not found /home/%s\n' "$UNIX_USER" 1>&2
-  exit 1
+	printf '[-] Error: directory not found /home/%s\n' "$UNIX_USER" 1>&2
+	exit 1
 fi
 
-script_dst="/home/$UNIX_USER/install_tools.sh"
-cp ./user/install_tools.sh "$script_dst" || \
-  cp ../user/install_tools.sh "$script_dst"
-chown "$UNIX_USER:$UNIX_USER" "$script_dst"
+# also install the setup script into the the users home dir
+# so it can be executed as the user there without root privileges
+cd "/home/$UNIX_USER"
+if [ ! -d setup-chiller-vps ]
+then
+	git clone git@github.com:ChillerDragon/setup-chiller-vps.git
+fi
+cd setup-chiller-vps
+git pull
+chown -R "$UNIX_USER:$UNIX_USER" "/home/$UNIX_USER/setup-chiller-vps"
 
-su - "$UNIX_USER" -c "/bin/bash $script_dst"
+su - "$UNIX_USER" -c "/bin/bash -c 'cd /home/$UNIX_USER/setup-chiller-vps && ./user/install_tools.sh'"
 
 printf '[*] done all tools installed for user %s.\n' "$UNIX_USER"
 
