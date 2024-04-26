e915238008685e76d7b419022bf97d3175ed4e49
ChillerDragon <ChillerDragon@gmail.com>
2024-04-26 09:51:01 +0800

    This is getting messy

--- root/harden_filesystem.sh
+++ root/harden_filesystem.sh
@@ -10,7 +10,7 @@ do
 	exit 1
 done
 
-restricted_dirs=(/usr/games /usr/src /media /opt /srv /var)
+restricted_dirs=(/usr/games /usr/src /media /opt /srv)
 
 printf '[*] removing read access for %s\n' "${restricted_dirs[*]}"
 chown -R root:admin "${restricted_dirs[@]}"
@@ -30,6 +30,23 @@ shopt -u extglob
 chmod o-r /etc/.*
 chmod o-x /etc/*/
 
+printf '[*] harden /var\n'
+chown root:admin /var
+# allow cd /var because a lot of tools need it like _apt or www-data
+# but disallow any sub directory
+# they have to be whitelisted for the users
+chmod o-x /var/*/
+chmod o-r /var/
+
+printf '[*] open /var/cache/apt\n'
+chmod o+x /var/cache
+chmod o+x /var/cache/apt
+
+# https://askubuntu.com/a/908825
+printf '[*] allow _apt user to use /var/cache\n'
+chown -Rv _apt:root /var/cache/apt/archives/partial/
+chmod -Rv 700 /var/cache/apt/archives/partial/
+
 printf '[*] allowing read access for /etc/alternatives and /etc/profile\n'
 chmod o+x /etc/alternatives
 chmod o+r /etc/profile
@@ -45,9 +62,4 @@ chmod o+x /etc/ssl
 chmod o+x /etc/ssl/certs
 chmod o-r /etc/ssl/certs/*
 chmod o+r /etc/ssl/certs/ca-certificates.crt
-
-# https://askubuntu.com/a/908825
-printf '[*] allow _apt user to use /var/cache\n'
-sudo chown -Rv _apt:root /var/cache/apt/archives/partial/
-sudo chmod -Rv 700 /var/cache/apt/archives/partial/
 
